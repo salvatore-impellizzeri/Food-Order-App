68207018db42f7547d20e23889460048dec6605b
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2025-01-10 21:49:48 +0100

    v 1.0

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,10 +5,25 @@ import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
 import Input from "./UI/Input";
+import useHttp from "../hooks/useHttp";
+import Error from './Error.jsx';
+
+const requestConfig = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+};
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext);
     const userProgressCtx = useContext(UserProgressContext);
+
+    const {data, isLoading, error, sendRequest, clearData} = useHttp(
+        'http://localhost:3000/orders', 
+        requestConfig,
+
+    );
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price ,0
@@ -17,24 +32,55 @@ export default function Checkout() {
     function handleClose() {
         userProgressCtx.hideCheckout();
     }
+
+    function handleFinish() {
+        userProgressCtx.hideCheckout();
+        cartCtx.clearCart();
+        clearData();
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
 
         const fd = new FormData(event.target);
         const customerData = Object.fromEntries(fd.entries());
 
-        fetch('http://localhost:3000/orders', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json' 
-            },
-            body: JSON.stringify({
+        sendRequest(
+            JSON.stringify({
                 order:{
                     items: cartCtx.items,
                     customer: customerData
                 }
             })
-        });
+        );
+    }
+
+    let actions = (
+        <>
+            <Button type="button" onClick={handleClose} textOnly>
+                Close
+            </Button>
+            <Button type="submit" >
+                Submit Order
+            </Button>
+        </>
+    );
+
+    if(isLoading) {
+        actions = <span>Sending order data...</span>
+    }
+
+    if(data && !error) {
+        return (
+            <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleFinish}>
+                <h2>Success!</h2>
+                <p>Your order was submitted successfully.</p>
+                <p>We will get back to you with more details via email within the next few minutes.</p>
+                <p className="modal-actions">
+                    <Button onClick={handleFinish}>Okay</Button>
+                </p>
+            </Modal>
+        );
     }
 
     return (
@@ -69,16 +115,13 @@ export default function Checkout() {
                         type="text"
                         id="city" 
                     />
-
-                    <p className="modal-actions">
-                        <Button type="button" onClick={handleClose} textOnly>
-                            Close
-                            </Button>
-                        <Button type="submit" >
-                            Submit Order
-                        </Button>
-                    </p>
                 </div>
+
+                {error && <Error title="Failed to submit order" message={error} />}
+
+                <p className="modal-actions">
+                    {actions}
+                </p>
             </form>
         </Modal>
     );
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -17,11 +17,15 @@ export default function useHttp(url, config, initialData) {
     const [error, setError] = useState();
     const [isLoading, setIsLoading] = useState(false);
 
+    function clearData() {
+        setData(initialData);
+    }
+
     const sendRequest = useCallback(
-        async function sendRequest() {
+        async function sendRequest(data) {
             setIsLoading(true);
             try{
-                const resData = await sendHttpRequest(url, config);
+                const resData = await sendHttpRequest(url, { ...config, body: data });
                 setData(resData);
             }  catch(error){
                 setError(error.message || 'Something went wrong!');
@@ -40,6 +44,7 @@ export default function useHttp(url, config, initialData) {
         data,
         isLoading,
         error,
-        sendRequest
+        sendRequest,
+        clearData
     };
 }
